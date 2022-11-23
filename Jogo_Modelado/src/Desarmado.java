7c8396575299ebedcaf636f7b8f6c14cb7579748
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2022-11-23 19:25:18 -0300

    Delete Jogo_Modelado directory

--- Jogo_Modelado/src/Desarmado.java
+++
@@ -1,9 +0,0 @@
-
-public class Desarmado implements Arma_IF{
-
-    @Override
-    public void usarArma() {
-        System.out.println("JAB DIRETO ESQUIVA ABAIXA GANCHO CRUZADO");
-    }
-    
-}
--- Jogo_Modelado/src/Fuzil.java
+++
@@ -1,9 +0,0 @@
-
-public class Fuzil implements Arma_IF{
-
-    @Override
-    public void usarArma() {
-        System.out.println("PRAPRAPRAPRA");
-    }
-    
-}
